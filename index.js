099634a51b0bb0572412f46b6c79df4809078f06
alvislim <luvhatehero1@gmail.com>
2024-05-20 11:17:44 +0800

    update cors

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const cors = require("cors");
 const passport = require("passport");
 
 const PORT = process.env.PORT || 4000;
-const corsURLs = [
-  "http://localhost:3000",
-  "https://buynowor.herokuapp.com",
-  "https://buynowbe.herokuapp.com/coldstorage",
-  "https://coldstorage.com.sg",
-  "https://www.allforyou.sg",
-];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,16 +23,7 @@ app.use(
   })
 );
 
-const options = [
-  cors({
-    origin: "*",
-    methods: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-];
-
-app.use(options);
+app.use(cors());
 
 app.use(passport.initialize());
 app.use(passport.session());
